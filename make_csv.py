1877f51694169c85b33240fcd770ab3dd92959ec
Sub Zero Project <45010593+subzeroproject@users.noreply.github.com>
2020-04-22 11:41:50 +0700

    Update make_csv.py

--- make_csv.py
+++ make_csv.py
@@ -1,6 +1,5 @@
 import json
 import csv
-import requests
 
 import requests
 link = requests.get("https://my-dev-shop-12.myshopify.com/admin/api/2020-04/customers.json", auth=('63250ac0afd68cccd81f5383228b83df', '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'))
